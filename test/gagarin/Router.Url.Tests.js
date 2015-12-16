4114b127bf1c52a72b31b469e4f3687853b6b3d4
Abigail Watson <awatson1978@gmail.com>
2015-12-15 21:01:00 -0800

    Router.Url.Tests.js

---
+++ test/gagarin/Router.Url.Tests.js
@@ -0,0 +1,31 @@
+// 
+// describe('clinical:router-url', function () {
+//   var server = meteor();
+//   var client = browser(server);
+//
+//   beforeEach(function () {
+//     server.execute(function () {
+//
+//     }).then(function (value){
+//
+//     });
+//   });
+//   afterEach(function () {
+//     server.execute(function () {
+//
+//     });
+//   });
+//
+//   it('Router should exist on the client', function () {
+//     return client.execute(function () {
+//       // expect(Router).to.exist;
+//     });
+//   });
+//
+//   it('Router should exist on the server', function () {
+//     return server.execute(function () {
+//       //expect(typeof Router).to.equal("undefined");
+//       expect(Router).to.exist;
+//     });
+//   });
+// });
