8b8c9bd83ebec1622fcc27b5df2e779ab39b7098
Abigail Watson <awatson1978@gmail.com>
2016-01-29 22:16:07 -0800

    Url should be undefined on server

--- tests/gagarin/Router.Url.Tests.js
+++ tests/gagarin/Router.Url.Tests.js
@@ -9,10 +9,10 @@ describe('clinical:router-url', function () {
     });
   });
 
-  it('Router should exist on the server', function () {
+  it('Router should be undefined on the server', function () {
     return server.execute(function () {
       //expect(typeof Router).to.equal("undefined");
-      expect(Url).to.not.exist;
+      expect(Url).to.be.undefined;
     });
   });
 });
