226b141d4f13bfd868a702fcd69a1f4c6b452a87
Abigail Watson <awatson1978@gmail.com>
2016-01-29 22:22:53 -0800

    undefined is a string

--- tests/gagarin/Router.Url.Tests.js
+++ tests/gagarin/Router.Url.Tests.js
@@ -3,16 +3,16 @@ describe('clinical:router-url', function () {
   var server = meteor();
   var client = browser(server);
 
-  it('Router should exist on the client', function () {
+  it('Url should exist on the client', function () {
     return client.execute(function () {
       expect(Url).to.exist;
     });
   });
 
-  it('Router should be undefined on the server', function () {
+  it('Url should be undefined on the server', function () {
     return server.execute(function () {
       //expect(typeof Router).to.equal("undefined");
-      expect(Url).to.be.undefined;
+      expect(Url).to.equal("undefined");
     });
   });
 });
